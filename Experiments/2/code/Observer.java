182f7d9c7cbd02f1256c29690324453b1fae401f
Julina00 <58408916+CleiZz@users.noreply.github.com>
2021-09-13 18:30:42 +0800

    Delete Experiments/2/code directory

--- Experiments/2/code/Observer.java
+++
@@ -1,5 +0,0 @@
-public interface Observer {
- 
-	public abstract void update(String id, double price);
-}
- 
--- Experiments/2/code/Subject.java
+++
@@ -1,6 +0,0 @@
-public interface Subject {
-	
-	public abstract void registerHolder(StockHolder holder);
-	public abstract void removeHolder(StockHolder  holder);
-	public abstract void notifyHolders();
-}
